8003ccd32f1e4306cbbccb7f6c93866b8b473f1b
ahrorbeksulaymanov <ahrorsulaymanov2@gmail.com>
2025-09-16 15:52:39 +0500

    menu navbar qilinyapti

--- src/components/BottomMenuBar.tsx
+++ src/components/BottomMenuBar.tsx
@@ -1,29 +1,29 @@
 'use client';
 
-import Image from "next/image";
+import './BottomMenuBar.css';
 
-const homeIcon = (fill: string = '#B3B3B3') => <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20" fill="none">
-    <g clipPath="url(#clip0_1214_171)">
-        <path d="M6.92864 0.713867H2.64293C1.85395 0.713867 1.21436 1.35346 1.21436 2.14244V6.42815C1.21436 7.21713 1.85395 7.85672 2.64293 7.85672H6.92864C7.71762 7.85672 8.35721 7.21713 8.35721 6.42815V2.14244C8.35721 1.35346 7.71762 0.713867 6.92864 0.713867Z" stroke={fill} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-        <path d="M18.3574 0.713867H14.0716C13.2827 0.713867 12.6431 1.35346 12.6431 2.14244V6.42815C12.6431 7.21713 13.2827 7.85672 14.0716 7.85672H18.3574C19.1463 7.85672 19.7859 7.21713 19.7859 6.42815V2.14244C19.7859 1.35346 19.1463 0.713867 18.3574 0.713867Z" stroke={fill} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-        <path d="M6.92864 12.1426H2.64293C1.85395 12.1426 1.21436 12.7822 1.21436 13.5711V17.8569C1.21436 18.6458 1.85395 19.2854 2.64293 19.2854H6.92864C7.71762 19.2854 8.35721 18.6458 8.35721 17.8569V13.5711C8.35721 12.7822 7.71762 12.1426 6.92864 12.1426Z" stroke={fill} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-        <path d="M18.3574 12.1426H14.0716C13.2827 12.1426 12.6431 12.7822 12.6431 13.5711V17.8569C12.6431 18.6458 13.2827 19.2854 14.0716 19.2854H18.3574C19.1463 19.2854 19.7859 18.6458 19.7859 17.8569V13.5711C19.7859 12.7822 19.1463 12.1426 18.3574 12.1426Z" stroke={fill} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+const homeIcon = (fill: string = '#B3B3B3') => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <g clip-path="url(#clip0_1784_5452)">
+        <path d="M7.71432 0.857422H2.57146C1.62469 0.857422 0.857178 1.62493 0.857178 2.57171V7.71456C0.857178 8.66134 1.62469 9.42885 2.57146 9.42885H7.71432C8.66109 9.42885 9.42861 8.66134 9.42861 7.71456V2.57171C9.42861 1.62493 8.66109 0.857422 7.71432 0.857422Z" stroke="#B3B3B3" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M21.4287 0.857422H16.2858C15.339 0.857422 14.5715 1.62493 14.5715 2.57171V7.71456C14.5715 8.66134 15.339 9.42885 16.2858 9.42885H21.4287C22.3754 9.42885 23.143 8.66134 23.143 7.71456V2.57171C23.143 1.62493 22.3754 0.857422 21.4287 0.857422Z" stroke="#B3B3B3" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M7.71432 14.5723H2.57146C1.62469 14.5723 0.857178 15.3398 0.857178 16.2866V21.4294C0.857178 22.3762 1.62469 23.1437 2.57146 23.1437H7.71432C8.66109 23.1437 9.42861 22.3762 9.42861 21.4294V16.2866C9.42861 15.3398 8.66109 14.5723 7.71432 14.5723Z" stroke="#B3B3B3" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M21.4287 14.5723H16.2858C15.339 14.5723 14.5715 15.3398 14.5715 16.2866V21.4294C14.5715 22.3762 15.339 23.1437 16.2858 23.1437H21.4287C22.3754 23.1437 23.143 22.3762 23.143 21.4294V16.2866C23.143 15.3398 22.3754 14.5723 21.4287 14.5723Z" stroke="#B3B3B3" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
     </g>
     <defs>
-        <clipPath id="clip0_1214_171">
-            <rect width="20" height="20" fill="white" transform="translate(0.5)" />
+        <clipPath id="clip0_1784_5452">
+            <rect width="24" height="24" fill="white" />
         </clipPath>
     </defs>
 </svg>;
 
-const editIcon = (fill: string = '#B3B3B3') => <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none">
-    <g clipPath="url(#clip0_1214_180)">
-        <path d="M10.0001 13.3572L5.71436 14.1286L6.42864 9.78577L14.6144 1.62862C14.7472 1.49473 14.9052 1.38845 15.0792 1.31592C15.2533 1.24339 15.4401 1.20605 15.6286 1.20605C15.8172 1.20605 16.004 1.24339 16.178 1.31592C16.3521 1.38845 16.5101 1.49473 16.6429 1.62862L18.1572 3.14291C18.2911 3.27571 18.3974 3.43372 18.4699 3.6078C18.5424 3.78188 18.5798 3.96861 18.5798 4.1572C18.5798 4.34578 18.5424 4.53251 18.4699 4.70659C18.3974 4.88067 18.2911 5.03868 18.1572 5.17148L10.0001 13.3572Z" stroke={fill} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-        <path d="M17.1429 14.0717V18.3574C17.1429 18.7363 16.9924 19.0997 16.7245 19.3676C16.4566 19.6355 16.0932 19.786 15.7144 19.786H2.14293C1.76405 19.786 1.40068 19.6355 1.13277 19.3676C0.864865 19.0997 0.714355 18.7363 0.714355 18.3574V4.78599C0.714355 4.40711 0.864865 4.04375 1.13277 3.77584C1.40068 3.50793 1.76405 3.35742 2.14293 3.35742H6.42864" stroke={fill} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+const editIcon = (fill: string = '#B3B3B3') => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <g clip-path="url(#clip0_1784_5460)">
+        <path d="M12 15.429L6.85718 16.3547L7.71432 11.1433L17.5372 1.35474C17.6965 1.19406 17.8861 1.06653 18.095 0.979497C18.3039 0.892465 18.528 0.847656 18.7543 0.847656C18.9806 0.847656 19.2047 0.892465 19.4136 0.979497C19.6225 1.06653 19.8121 1.19406 19.9715 1.35474L21.7886 3.17188C21.9493 3.33125 22.0768 3.52085 22.1638 3.72975C22.2509 3.93865 22.2957 4.16272 22.2957 4.38902C22.2957 4.61533 22.2509 4.8394 22.1638 5.0483C22.0768 5.2572 21.9493 5.4468 21.7886 5.60617L12 15.429Z" stroke="#B3B3B3" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M20.5715 16.2868V21.4297C20.5715 21.8843 20.3908 22.3204 20.0694 22.6419C19.7479 22.9634 19.3118 23.144 18.8572 23.144H2.57146C2.11681 23.144 1.68077 22.9634 1.35928 22.6419C1.03779 22.3204 0.857178 21.8843 0.857178 21.4297V5.14397C0.857178 4.68932 1.03779 4.25328 1.35928 3.93179C1.68077 3.6103 2.11681 3.42969 2.57146 3.42969H7.71432" stroke="#B3B3B3" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
     </g>
     <defs>
-        <clipPath id="clip0_1214_180">
-            <rect width="20" height="20" fill="white" transform="translate(0 0.5)" />
+        <clipPath id="clip0_1784_5460">
+            <rect width="24" height="24" fill="white" />
         </clipPath>
     </defs>
 </svg>
@@ -42,40 +42,41 @@ const profileIcon = (fill: string = '#B3B3B3') => <svg xmlns="http://www.w3.org/
 
 const BottomMenuBar = () => {
     return (
-        <div className="h-[112px] mt-auto">
-            <div className="bg-[#ff8e8e] fixed bottom-0 w-full sm:max-w-sm rounded-t-[6px] px-2 z-10 -mt-4 pb-2">
+        <div className="h-[112px] mt-auto navbar-menu">
+            <div className="bg-[#fff] fixed bottom-0 w-full sm:max-w-sm rounded-t-[6px] px-2 z-10 -mt-4 pb-2" style={{ boxShadow: "0 -1px 2px 0 rgba(0,0,0,0.04)" }}>
                 <div className="flex items-center justify-between">
                     {/* Home */}
                     <div className="px-3 py-2.5 flex flex-col items-center cursor-pointer">
                         {homeIcon("#4E2A18")}
-                        <span className="text-[8px] text-primary mt-1">홈</span>
+                        <span className="text-[12px] text-primary mt-2">홈</span>
                     </div>
 
                     {/* edit */}
-                    <div className="px-3 py-2.5 flex flex-col items-center cursor-pointer mr-16">
+                    <div className="px-3 py-2.5 flex flex-col items-center cursor-pointer">
                         {editIcon()}
-                        <span className="text-[8px] text-[#999] mt-1">리뷰</span>
+                        <span className="text-[12px] text-[#999] mt-2">리뷰</span>
                     </div>
 
-                    
-                    <div className="navbar-menu-main-item absolute cursor-pointer right-[50%] translate-x-[50%] -translate-y-[26%] flex flex-col items-center justify-center bg-action-primary rounded-full w-[64px] h-[64px]" style={{boxShadow: "0 4px 12px 0 rgba(78,42,24,0.50)"}}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
-                            <path d="M10.8735 5.84853C10.6702 5.39222 10.4944 4.68897 11.412 4.26488C12.2142 3.8891 13.3462 3.76562 14.533 3.76562C16.0386 3.78173 17.5771 4.13604 19.0607 4.80171C20.5443 5.46738 21.9454 6.43367 23.1817 7.64154C24.4181 8.84941 25.4676 10.2827 26.2698 11.8557C27.072 13.4286 27.6105 15.1089 27.8523 16.7999C28.094 18.4909 28.0391 20.1658 27.6874 21.7226C27.3358 23.2794 26.6599 24.6215 25.8027 25.8777C25.3302 26.5058 24.7862 27.0694 24.1873 27.5472C23.3081 28.2505 22.2147 28.1216 21.6542 26.9997L16.3848 16.6603L10.8735 5.84853Z" fill="white" />
-                            <path d="M17.2419 22.4851C17.5056 23.0058 17.5551 23.4138 16.7144 23.8218C15.7418 24.2888 14.5989 24.3479 13.467 24.3371C11.9614 24.321 10.4229 23.9667 8.93933 23.301C7.45575 22.6354 6.05459 21.6691 4.81827 20.4612C3.58196 19.2533 2.53246 17.82 1.73023 16.2471C0.928 14.6742 0.389516 12.9939 0.147747 11.3029C-0.0940211 9.61186 -0.0390737 7.93695 0.31259 6.38014C0.664253 4.82333 1.30164 3.41146 2.19728 2.22507C2.71928 1.53256 3.31821 0.925939 3.98857 0.41595C4.78531 -0.190669 5.70842 -0.212142 6.24141 0.904466L11.6098 11.4424L17.2364 22.4904L17.2419 22.4851Z" fill="white" />
-                        </svg>
-                        <span className="text-[12px] text-[#fff] mt-0.5">내 커피</span>
+                    <div className='navbar-menu-main-item-wrapper relative shrink-0 flex-col items-center justify-center -translate-y-[36%] rounded-full'>
+                        <div className="navbar-menu-main-item absolute cursor-pointer flex flex-col items-center justify-center bg-action-primary rounded-full w-[64px] h-[64px]" style={{ boxShadow: "0 4px 12px 0 rgba(78,42,24,0.50)" }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
+                                <path d="M10.8735 5.84853C10.6702 5.39222 10.4944 4.68897 11.412 4.26488C12.2142 3.8891 13.3462 3.76562 14.533 3.76562C16.0386 3.78173 17.5771 4.13604 19.0607 4.80171C20.5443 5.46738 21.9454 6.43367 23.1817 7.64154C24.4181 8.84941 25.4676 10.2827 26.2698 11.8557C27.072 13.4286 27.6105 15.1089 27.8523 16.7999C28.094 18.4909 28.0391 20.1658 27.6874 21.7226C27.3358 23.2794 26.6599 24.6215 25.8027 25.8777C25.3302 26.5058 24.7862 27.0694 24.1873 27.5472C23.3081 28.2505 22.2147 28.1216 21.6542 26.9997L16.3848 16.6603L10.8735 5.84853Z" fill="white" />
+                                <path d="M17.2419 22.4851C17.5056 23.0058 17.5551 23.4138 16.7144 23.8218C15.7418 24.2888 14.5989 24.3479 13.467 24.3371C11.9614 24.321 10.4229 23.9667 8.93933 23.301C7.45575 22.6354 6.05459 21.6691 4.81827 20.4612C3.58196 19.2533 2.53246 17.82 1.73023 16.2471C0.928 14.6742 0.389516 12.9939 0.147747 11.3029C-0.0940211 9.61186 -0.0390737 7.93695 0.31259 6.38014C0.664253 4.82333 1.30164 3.41146 2.19728 2.22507C2.71928 1.53256 3.31821 0.925939 3.98857 0.41595C4.78531 -0.190669 5.70842 -0.212142 6.24141 0.904466L11.6098 11.4424L17.2364 22.4904L17.2419 22.4851Z" fill="white" />
+                            </svg>
+                            <span className="text-[12px] text-[#fff] mt-0.5">내 커피</span>
+                        </div>
                     </div>
 
                     {/* globus */}
-                    <div className="px-3 py-2.5 flex flex-col items-center cursor-pointer ml-16">
+                    <div className="px-3 py-2.5 flex flex-col items-center cursor-pointer">
                         {globalIcon()}
-                        <span className="text-[8px] text-[#999] mt-1">커뮤니티</span>
+                        <span className="text-[12px] text-[#999] mt-2">커뮤니티</span>
                     </div>
 
                     {/* Profile */}
                     <div className="px-3 py-2.5 flex flex-col items-center cursor-pointer">
                         {profileIcon()}
-                        <span className="text-[8px] text-[#999] mt-1">MY</span>
+                        <span className="text-[12px] text-[#999] mt-2">MY</span>
                     </div>
                 </div>
             </div>
