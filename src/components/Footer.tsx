81a053632a9aea40db1ade01f5cfb57c3e5ed954
ahrorbeksulaymanov <ahrorsulaymanov2@gmail.com>
2025-09-29 10:52:53 +0500

    ss

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,10 +11,11 @@ const Footer = () => {
                     <p>대표 : 노환걸</p>
                     <p>18530 경기도 화성시 팔탄면 서해로987번길 23 (주)아로마빌커피</p>
                     <p>사업자등록번호 : 122-81-68406</p>
+                    <p>대표전화번호 : 070-7721-8181</p>
                     <p>통신판매업 : 제 2018-화성팔탄-0052호</p>
                     <p>이메일 : aromaville@aromaville.co.kr</p>
                     <p>개인정보관리책임 : 노환결 (aromaville@aromaville.co.kr)</p>
-                    <p>Copyright ⓒ MyCoffee.AI All Rights Reserved.</p>
+                    <p>Copyright ⓒ MyCoffee.AI All Rights Reserved.</p>
                     <p>이용약관  개인정보취급방침</p>
                 </div>
             </div>
