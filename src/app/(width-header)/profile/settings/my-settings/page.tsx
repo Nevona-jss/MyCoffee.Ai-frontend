f2c8f6280f20a22083212d67d465aa1a0ea3def5
fsodiqov <foziljonsodiq@gmail.com>
2025-09-29 09:34:38 +0500

    my-settings page text edited

--- src/app/(width-header)/profile/settings/my-settings/page.tsx
+++ src/app/(width-header)/profile/settings/my-settings/page.tsx
@@ -4,8 +4,8 @@ import React, { useEffect } from "react";
 import Link from "next/link";
 import { useHeaderStore } from "@/stores/header-store";
 
-const MySettings = () => { 
-  const { setHeader } = useHeaderStore(); 
+const MySettings = () => {
+  const { setHeader } = useHeaderStore();
   useEffect(() => {
     setHeader({
       title: "설정",
@@ -17,26 +17,30 @@ const MySettings = () => {
       {/* Management List */}
       <div className="bg-white rounded-2xl border border-border-default p-3">
         {/* Payment Method Management */}
-        <Link href="/profile/settings/notification-settings" className="flex items-center justify-between py-1.5">
+        <Link
+          href="/profile/settings/notification-settings"
+          className="flex items-center justify-between py-1.5"
+        >
           <div className="flex items-center gap-2">
             <div className="w-8 h-8 bg-brand-secondary-accent-sub rounded-full flex items-center justify-center">
               <Bell size={20} className="text-action-primary" />
             </div>
-            <span className="text-sm leading-[20px] font-bold">
-              결제 수단 관리
-            </span>
+            <span className="text-sm leading-[20px] font-bold">알림 설정</span>
           </div>
           <ChevronRight size={20} className="text-icon-default" />
         </Link>
 
         {/* Delivery Address Management */}
-        <Link href="/profile/settings/apply-term-of-use" className="flex items-center justify-between py-1.5">
+        <Link
+          href="/profile/settings/apply-term-of-use"
+          className="flex items-center justify-between py-1.5"
+        >
           <div className="flex items-center gap-2">
             <div className="w-8 h-8 bg-brand-secondary-accent-sub rounded-full flex items-center justify-center">
               <ShieldAlert size={20} className="text-action-primary" />
             </div>
             <span className="text-sm leading-[20px] font-bold">
-              배송지 관리
+              약관 및 개인정보 처리 동의
             </span>
           </div>
           <ChevronRight size={20} className="text-icon-default" />
