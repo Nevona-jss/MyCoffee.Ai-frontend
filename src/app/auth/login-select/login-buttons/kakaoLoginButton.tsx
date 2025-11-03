42e9a90197aa00a36a43b80fcb5502e9273991c4
ahrorbeksulaymanov <ahrorsulaymanov2@gmail.com>
2025-11-03 09:47:58 +0500

    kakao talk is integrated

--- src/app/auth/login-select/login-buttons/kakaoLoginButton.tsx
+++ src/app/auth/login-select/login-buttons/kakaoLoginButton.tsx
@@ -2,7 +2,8 @@
 const KakaoTalkLoginButton = () => {
 
     const REST_API_KEY = "9e333b47e27e4b67233780b5fe1e0abf";
-    const REDIRECT_URI = `https://mycoffeeai.com/home`;
+    const REDIRECT_URI = `https://dev.mycoffeeai.com/auth/kakao/callback`;
+    // const REDIRECT_URI = `https://mycoffeeai.com/home`;
 
     const loginWithCacaouTalk = () => {
         const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
